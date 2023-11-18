f9a5f9ed9c5eefbf80994dcad9f42dc3b6545fbf
greenboy <74439927+truefrog@users.noreply.github.com>
2023-11-17 20:45:43 -0600

    fixed some next configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  exportTrailingSlash: true,
   images: {
     domains: ['images.unsplash.com'],
   },
+  distDir: 'out',
   // swcMinify: true,
 }
 
